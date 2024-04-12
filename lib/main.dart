003dbb7ae65db5d53f3daeaea1fbf4a8c8c84f9d
Gyanendra Kumar <gyanendrak874@gmail.com>
2024-04-12 11:29:16 +0530

    Added basic structure for todo

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:todo/pages/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+        primaryColor: Colors.yellow,
+      ),
+    );
+  }
+}
