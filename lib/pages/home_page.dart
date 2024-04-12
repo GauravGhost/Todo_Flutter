e4420903eabae8f1bb51c5e6918437de873d1b20
Gyanendra Kumar <gyanendrak874@gmail.com>
2024-04-12 12:10:34 +0530

    Added todo list with checkbox

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo/utils/todo_tile.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,6 +9,15 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List todoList = [
+    ["Make Tutorial", false],
+    ["Do Excercise", false]
+  ];
+  void checkboxChanged(bool? value, int index) {
+    setState(() {
+      todoList[index][1] = !todoList[index][1];
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +27,16 @@ class _HomePageState extends State<HomePage> {
         title: Text('TO DO'),
         centerTitle: true,
       ),
-      body: ListView(children: [],),
+      body: ListView.builder(
+        itemCount: todoList.length,
+        itemBuilder: (context, index) {
+          return TodoTile(
+            taskName: todoList[index][0],
+            taskCompleted: todoList[index][1],
+            onChanged: (value) => checkboxChanged(value, index),
+          );
+        },
+      ),
     );
   }
 }
